51df740a75655a3dfcd62be38966baacd7a2a4dc
武田 <ichigo_happy@ezweb.ne.jp>
2019-01-17 10:06:46 +0900

    きれいにした 当たり判定のよこバグ直し

--- source/block.h
+++ source/block.h
@@ -10,9 +10,9 @@ using namespace SimpleMath;
 //構造体
 typedef struct
 {
-	int index[2];//配列番号//0がX座標・・j、１がY座標・・i
-	int color;//色情報
-	bool is_wall;//壁かどうか
+    int index[ 2 ];//配列番号//0がX座標・・j、１がY座標・・i
+    int color;//色情報
+    bool is_wall;//壁かどうか
 }block;
 
 class Block
@@ -20,27 +20,27 @@ class Block
 private:
 
     ID3D11ShaderResourceView* texture_;//テクスチャ
-   
+
     int key_state;//何のキーを押したかを格納
-	int count;
-	block falling_block[4][4];//今落ちているブロック
+    int count;
+    block falling_block[ 4 ][ 4 ];//今落ちているブロック
     block field[ 22 ][ 12 ];//固定フィールド
     Vector2 position2; //動いてるブロック描画座標
     Vector2 position_;//固定ブロック描画座標
-	bool exist_fallingblock;//ブロック生成用フラグ
+    bool exist_fallingblock;//ブロック生成用フラグ
 
-    
+
 public:
     Block();   //コンストラクタ
     bool init();    //初期化
     void update();  //更新
     void draw();    //描画    
     void destroy(); //破棄
-	bool can_move(int direction);//落ちれるかどうか
+    bool can_move( int direction );//落ちれるかどうか
     void Copy_fallingblock_in_field();//fallingblockをフィールドにコピーする
-    void Delete_fieldblock(int num);//フィールドのそろったブロックを消す処理
-	void move_down();//下の場合の処理(自動落下も含む）
-    void Drop_fieldblock(int no);//ブロックが消えた後に、残っているブロックを下に落とす処理
+    void Delete_fieldblock( int num );//フィールドのそろったブロックを消す処理
+    void move_down();//下の場合の処理(自動落下も含む）
+    void Drop_fieldblock( int no );//ブロックが消えた後に、残っているブロックを下に落とす処理
     void Make_fallingblock();//落ちるブロックを作る
-	void init_fallingblock();//落ちるブロックの初期化
+    void init_fallingblock();//落ちるブロックの初期化
 };
--- source/button.cpp
+++ source/button.cpp
@@ -11,15 +11,15 @@ const int Buttonsize = 128;//A,B,LBボタンのサイズ
 //列挙体
 enum
 {
-None,
-Up,
-RightUp,
-Right,
-RightDown,
-Down,
-LeftDown,
-Left,
-LeftUp
+    None,
+    Up,
+    RightUp,
+    Right,
+    RightDown,
+    Down,
+    LeftDown,
+    Left,
+    LeftUp
 };
 
 //コンストラクタ
@@ -53,7 +53,7 @@ bool Button::init()
         return false;
     }
     return true;
-  
+
 }
 
 //更新
@@ -63,16 +63,16 @@ void Button::update()
     GamePad::State pad = Pad::getState();
 
     //十字キー
-    if( (key.Up&&key.Left)||(pad.dpad.up&&pad.dpad.left ))
+    if( (key.Up&&key.Left) || (pad.dpad.up&&pad.dpad.left) )
     {
         pattern = LeftUp;
-        
+
     }
-    else if( (key.Up&&key.Right)||(pad.dpad.up&&pad.dpad.right) )
+    else if( (key.Up&&key.Right) || (pad.dpad.up&&pad.dpad.right) )
     {
         pattern = RightUp;
     }
-    else if( (key.Down&&key.Right)||(pad.dpad.down&&pad.dpad.right) )
+    else if( (key.Down&&key.Right) || (pad.dpad.down&&pad.dpad.right) )
     {
         pattern = RightDown;
     }
@@ -80,19 +80,19 @@ void Button::update()
     {
         pattern = LeftDown;
     }
-    else if( key.Right||pad.dpad.right )
+    else if( key.Right || pad.dpad.right )
     {
         pattern = Right;
     }
-    else if( key.Left||pad.dpad.left )
+    else if( key.Left || pad.dpad.left )
     {
         pattern = Left;
     }
-    else if( key.Down||pad.dpad.down )
+    else if( key.Down || pad.dpad.down )
     {
         pattern = Down;
     }
-    else if( key.Up||pad.dpad.up )
+    else if( key.Up || pad.dpad.up )
     {
         pattern = Up;
     }
@@ -101,27 +101,27 @@ void Button::update()
 
 
     //Aボタン
-    if( pad.buttons.a||key.A)
+    if( pad.buttons.a || key.A )
     {
         abutton = true;
     }
     else
         abutton = false;
     //Bボタン
-    if( pad.buttons.b||key.Z)
+    if( pad.buttons.b || key.Z )
     {
         bbutton = true;
     }
     else
         bbutton = false;
     //LBボタン
-    if( pad.buttons.leftShoulder||key.Enter )
+    if( pad.buttons.leftShoulder || key.Enter )
     {
         lbbutton = true;
     }
     else lbbutton = false;
     //PAUSEボタン
-    if( pad.buttons.menu||key.Space )
+    if( pad.buttons.menu || key.Space )
     {
         pausebutton = true;
     }
@@ -135,8 +135,8 @@ void Button::range()
     //矢印キー
     switch( pattern )
     {
-    case None:sizeleft=Arrowsize*None; break;
-    case Up:sizeleft =Arrowsize*Up; break;
+    case None:sizeleft = Arrowsize * None; break;
+    case Up:sizeleft = Arrowsize * Up; break;
     case RightUp:sizeleft = Arrowsize * RightUp; break;
     case RightDown:sizeleft = Arrowsize * RightDown; break;
     case Right:sizeleft = Arrowsize * Right; break;
@@ -183,39 +183,39 @@ void Button::draw()
     rect.top = 720;
     rect.left = sizeleft;
     rect.right = sizeleft + Arrowsize;
-    rect.bottom = rect.top+ Arrowsize;
+    rect.bottom = rect.top + Arrowsize;
 
     Sprite::draw( texture_, position, &rect );
-    
+
     //Aボタン描画
     RECT Arect;
     Arect.top = 464 + Asize;
-    Arect.left = 1536 ;
+    Arect.left = 1536;
     Arect.right = Arect.left + Buttonsize;
     Arect.bottom = Arect.top + Buttonsize;
 
     Sprite::draw( texture_, Aposition, &Arect );
 
     RECT Brect;
-        Brect.top = 464 + Bsize;
-        Brect.left = 1664;
-        Brect.right = Brect.left + Buttonsize;
-        Brect.bottom = Brect.top + Buttonsize;
-        Sprite::draw( texture_, Bposition, &Brect );
+    Brect.top = 464 + Bsize;
+    Brect.left = 1664;
+    Brect.right = Brect.left + Buttonsize;
+    Brect.bottom = Brect.top + Buttonsize;
+    Sprite::draw( texture_, Bposition, &Brect );
 
 
-        RECT LBrect;
-        LBrect.top =  464+ LBsize;
-        LBrect.left = 1280;
-        LBrect.right = LBrect.left + Buttonsize;
-        LBrect.bottom = LBrect.top + Buttonsize;
-        Sprite::draw( texture_, LBposition, &LBrect );
-        RECT Prect;
-        Prect.top = 336 + pausesize;
-        Prect.left = 1920;
-        Prect.right = Prect.left + 128;
-       Prect.bottom = Prect.top + 64;
-        Sprite::draw( texture_, Pposition, &Prect );
+    RECT LBrect;
+    LBrect.top = 464 + LBsize;
+    LBrect.left = 1280;
+    LBrect.right = LBrect.left + Buttonsize;
+    LBrect.bottom = LBrect.top + Buttonsize;
+    Sprite::draw( texture_, LBposition, &LBrect );
+    RECT Prect;
+    Prect.top = 336 + pausesize;
+    Prect.left = 1920;
+    Prect.right = Prect.left + 128;
+    Prect.bottom = Prect.top + 64;
+    Sprite::draw( texture_, Pposition, &Prect );
 }
 
 
